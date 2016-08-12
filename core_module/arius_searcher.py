23975433fbf254220e3e33075650e726bdc1d05a
Taras Zelyk <taras.zelyk@gmail.com>
2016-08-12 13:36:03 +0300

    Big refactoring

--- core_module/arius_searcher.py
+++ core_module/arius_searcher.py
@@ -3,29 +3,47 @@ from elasticsearch import Elasticsearch
 import sys
 sys.path.append("../")
 from configure import ConstExtractor
+_settings = ConstExtractor()
 
 
 class ESearchClient:
     """Simple class to search some data in the ElasticSearch database"""
 
     def __init__(self):
-        self._settings = ConstExtractor()
-        self.es = Elasticsearch(self._settings.getValue("elastic_host"))
+
+        self._host = _settings.getValue("elastic_host")
+        self._index = _settings.getValue("elastic_index")
+        self._type = _settings.getValue("elastic_type")
+        self._es = Elasticsearch(self._host)
 
     def search(self, query):
-        """Searches for all documents that are relevant to the query. 
+        """Searches for all documents that are relevant to the query.
         Returns a sorted by relevancy list of results, where every item is a tuple(path_to_file, score)"""
-        response = self.es.search(index=self._settings.getValue("elastic_index"), doc_type=self._settings.getValue("elastic_type"),
-                                  body={"query": {"match": {"file.content": query}}})
+        queryBody = {
+            "query": {
+                "bool": {
+                    "should": [
+                        {
+                            "match": {
+                                "file.content": queryItem
+                            }
+                        } for queryItem in query.split()
+                    ],
+                    "minimum_number_should_match": "2<75%"
+                }
+            }
+        }
+
+        response = self._es.search(index=self._index, doc_type=self._type, body=queryBody)
         result = []
         for doc in response['hits']['hits']:
             result.append((doc['_source']['title'], doc['_score']))
         return result
 
 if __name__ == '__main__':
-    query = 'softserve'
-    #query = "ARIUS"
-    #query = "html"
-    #query = 'Linux'
+    query = 'softserve arius'
+    # query = "ARIUS"
+    # query = "html"
+    # query = 'Linux'
     AS = ESearchClient()
     print(AS.search(query))
