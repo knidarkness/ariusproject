53ff8c16bdedfed533293918d0fa0d3651b5fb72
Sergey Dubovyk <dubovyk@ucu.edu.ua>
2016-08-25 08:25:54 +0300

    fixed deleted core.py file

---
+++ core_module/core.py
@@ -0,0 +1,195 @@
+import threading
+import sys
+import time
+import os
+from statemachine import statemachine
+sys.path.append("../")
+from client import RESTClient
+from sense_extraction import SenseExtractor
+from arius_searcher import ESearchClient
+from configure import Config
+cfg = Config()
+TAG = "[Core]"
+
+
+class Updater(threading.Thread):
+    def __init__(self, lock):
+        threading.Thread.__init__(self)
+        self._lock = lock
+
+        self._connection = RESTClient(cfg.get("core_server_input_address"),
+                                      cfg.get("core_server_input_port"),
+                                      cfg.get("core_server_input_url"))
+        self.input_speech = None
+
+    def run(self):
+        while True:
+            if not self.input_speech:
+                command = self._connection.GET_request(True, 0)
+                if command['speech_text'] != 'no updates':
+                    print '==================================================='
+                    print TAG, 'RECEIVED COMMAND'
+                    self._lock.acquire()
+                    self.input_speech = command['speech_text']
+                    self.new_input = True
+                    self._lock.release()
+
+            time.sleep(.1)
+
+
+class Core(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self._lock = threading.RLock()
+        self._statemachine = statemachine
+        cfg = Config()
+        self._updater = Updater(self._lock)
+
+        self._result_sender = RESTClient(cfg.get("core_server_output_address"),
+                                         cfg.get("core_server_output_port"),
+                                         cfg.get("core_server_output_url"))
+
+        self._ESclient = ESearchClient()
+        self._sense_extractor = SenseExtractor('stop.txt')
+
+        self._start_phrases = ['ok arius', 'what is that', 'what the fuck']
+        self._commands = {
+            "ZOOM_IN": ['zoom in', 'increase', 'enlarge', 'zoom more'],
+            "ZOOM_OUT": ['shrink', 'decrease', 'zoom less', 'zoom out'],
+            "NO_ZOOM": ['normal size', 'zero zoom', 'no zoom', 'zoom reset', 'reset zoom'],
+            "SCROLL_DOWN": ['page down', 'scroll down'],
+            "SCROLL_UP": ['page up', 'scroll up'],
+            "CANCEL": ['cancel', 'bye', 'thanks'],
+            "WAIT": ['wait']
+        }
+
+        self._commnad = None
+
+    def run(self):
+        self._updater.start()
+        data = {'type': 'OPEN_SCREEN', 'command': 'IDLE'}
+        self._send_command(data)
+        while True:
+            if self._updater.input_speech:
+                print TAG, 'state in the beginning is {}'.format(self._statemachine.get_state())
+                user_input = self._updater.input_speech
+                self._lock.acquire()
+                self._updater.input_speech = None
+                self._updater.new_input = False
+                self._lock.release()
+
+                if self._recognize_command(user_input) == 'CANCEL':
+                    self._statemachine.handle_message('cancel')
+                    request = {'type': 'OPEN_SCREEN', 'command': 'IDLE'}
+                    self._send_command({'type': 'SPEAK', 'command': 'Operation cancelled'})
+                    self._send_command(request)
+                    continue
+                if self._statemachine.get_state() == 'displaying_data':
+                    if self._recognize_command(user_input) == 'ZOOM_IN':
+                        request = {'type': 'ZOOM_IN', 'command': ''}
+                        self._send_command({'type': 'SPEAK', 'command': 'Enlarging'})
+                        self._send_command(request)
+                        continue
+                    elif self._recognize_command(user_input) == 'ZOOM_OUT':
+                        request = {'type': 'ZOOM_OUT', 'command': ''}
+                        self._send_command({'type': 'SPEAK', 'command': 'Zooming out'})
+                        self._send_command(request)
+                        continue
+                    elif self._recognize_command(user_input) == 'SCROLL_DOWN':
+                        request = {'type': 'SCROLL_DOWN', 'command': ''}
+                        self._send_command({'type': 'SPEAK', 'command': 'Scrolling down, sir'})
+                        self._send_command(request)
+                        continue
+                    elif self._recognize_command(user_input) == 'SCROLL_UP':
+                        request = {'type': 'SCROLL_UP', 'command': ''}
+                        self._send_command({'type': 'SPEAK', 'command': 'Scrolling up as you wish'})
+                        self._send_command(request)
+                        continue
+
+                if (self._statemachine.get_state() == 'idle' and self._recognize_start_phrase(user_input)) \
+                        or self._statemachine.get_state() != 'idle':
+                    for start in self._start_phrases:
+                        if start in user_input:
+                            user_input = user_input.replace(start, '')
+                    for word in user_input.split():
+                        if word in [i for j in self._commands.values() for i in j]:
+                            user_input = user_input.replace(word, '')
+
+                    query = ' '.join(self._sense_extractor.get_context(user_input))
+                    print TAG, 'Proceeding search query...'
+                    if query:
+                        self._statemachine.handle_message('request')
+                        self._send_command({'type': 'SPEAK', 'command': 'Search request accepted, my lord'})
+                        self._do_work(self._find_data, query)
+                    else:
+                        print TAG, 'search query is empty'
+                print TAG, 'state in the end is {}'.format(self._statemachine.get_state())
+
+            else:
+                pass
+                # print 'no commands'
+            time.sleep(.1)
+
+    def _recognize_command(self, data):
+        for command_key in self._commands.keys():
+            for command in self._commands[command_key]:
+                if command in data:
+                    print TAG, "Recognized command is {}, {}".format(command_key, type(command_key))
+                    return str(command_key)
+
+    def _recognize_start_phrase(self, data):
+        for start in self._start_phrases:
+            if start in data:
+                return True
+        return False
+
+    def _find_data(self, request, result):
+        print TAG, 'Searching data...'
+        data = {'type': 'OPEN_SCREEN', 'command': 'SEARCH'}
+        self._send_command(data)
+        data = self._ESclient.search(request)
+        print TAG, 'Opening search screen.'
+        time.sleep(7)
+        print TAG, data
+        if data:
+            file_ext = os.path.splitext(data[0][0])[1]
+            print TAG, 'File extension:', file_ext
+            if file_ext == '.pdf':
+                data = {'type': 'OPEN_PDF', 'command': data[0][0]}
+                self._statemachine.handle_message('found')
+            elif file_ext == '.html':
+                data = {'type': 'OPEN_LOCAL_PAGE', 'command': data[0][0]}
+                self._statemachine.handle_message('found')
+            elif file_ext == '.url':
+                data = open(cfg.get('output_server_home') + data[0][0])
+                link = data.readlines()[0]
+                print TAG, link
+                data = {'type': 'OPEN_URL', 'command': link}
+                self._statemachine.handle_message('found')
+
+        else:
+            data = {'type': 'OPEN_SCREEN', 'command': 'ERROR'}
+            self._statemachine.handle_message('not_found')
+        result.append(data)
+        return None
+
+    def _do_work(self, function, argument):
+        result = []
+        print TAG, argument
+        worker = threading.Thread(target=function, args=(argument, result))
+        worker.start()
+        while not (self._updater.new_input or result):
+            pass
+        if result:
+            self._send_command(result[0])
+            result = None
+            return None
+        else:
+            print TAG, 'Cancelled'
+            return None
+
+    def _send_command(self, command):
+        print TAG, 'Send command to Output module: ', command
+        self._result_sender.send_data_in_POST(command)
+core = Core()
+core.start()
