1194d67a3bd8ce67671915c408415698bfa616f6
Tartar-san <tarasov@ucu.edu.ua>
2016-08-25 15:29:16 +0300

    marytts & elastic in required_packages

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-#gnome-terminal -e marytts/bin/marytts-server
-#gnome-terminal -e elasticsearch/bin/elasticsearch
+#gnome-terminal -e required_packages/marytts/bin/marytts-server
+#gnome-terminal -e required_packages/elasticsearch/bin/elasticsearch
 cd additional_scripts
 gnome-terminal -e ./server.sh
 sleep 10 
