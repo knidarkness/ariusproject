6fd985b2287f217ac5d83643a89cbd31bea54485
Tartar-san <tarasov@ucu.edu.ua>
2016-08-26 14:54:00 +0300

    fix path

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-#gnome-terminal -e required_packages/marytts-5.1.2/bin/marytts-server
-#gnome-terminal -e required_packages/elasticsearch/bin/elasticsearch
+gnome-terminal -e required_packages/marytts-5.1.2/bin/marytts-server
+gnome-terminal -e required_packages/elasticsearch-2.3.5/bin/elasticsearch
 cd additional_scripts
 gnome-terminal -e ./server.sh
 sleep 10 
